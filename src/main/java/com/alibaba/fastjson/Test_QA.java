497164e0cacaace97e45a3fc188fa116c7a3e34a
EladSalti <elad.salti7@gmail.com>
2021-05-06 16:06:05 +0300

    first commit

---
+++ src/main/java/com/alibaba/fastjson/Test_QA.java
@@ -0,0 +1,18 @@
+package com.alibaba.fastjson;
+
+import com.google.common.util.concurrent.AtomicDouble;
+import com.google.common.util.concurrent.AtomicDoubleArray;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+import javax.servlet.ServletRequest;
+
+public class Test_QA {
+    public static void Main() {
+        AtomicDoubleArray ada = new AtomicDoubleArray(null);
+        ada.addAndGet(4,8);
+
+
+
+    }
+}
